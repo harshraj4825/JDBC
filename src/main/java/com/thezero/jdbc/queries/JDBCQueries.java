79ab8f7034d25bfa5b258b22a486fc69e98c8019
harsh <iitianharshraj@gmail.com>
2023-02-27 01:11:41 +0530

    Improving code with try with resource so that connection resource will close perfectly

--- src/main/java/com/thezero/jdbc/queries/JDBCQueries.java
+++ src/main/java/com/thezero/jdbc/queries/JDBCQueries.java
@@ -7,26 +7,22 @@ import java.sql.Statement;
 
 public class JDBCQueries {
     public static void main(String[] args) {
-        new JDBCQueries().testQuery("select * from employees");
+        new JDBCQueries().testQuery();
     }
-    private void testQuery(String query){//thezeroschema
-        Connection myConnection;
+    private void testQuery(){//thezeroschema
         Statement myStmt;
         ResultSet myRs;
-
         String dbURL="jdbc:mysql://localhost:3306/thezeroschema";
         String user="harsh";
         String pass="thezero@123";
-        try{
-            myConnection=DriverManager.getConnection(dbURL,user,pass);
-            myStmt=myConnection.createStatement();
-            myRs=myStmt.executeQuery(query);
-            while (myRs.next()){
-                System.out.println(myRs.getString("last_name")+" "+myRs.getString("first_name"));
+        try (Connection myConnection = DriverManager.getConnection(dbURL, user, pass)) {
+            myStmt = myConnection.createStatement();
+            myRs = myStmt.executeQuery("select * from employees");
+            while (myRs.next()) {
+                System.out.println(myRs.getString("last_name") + " " + myRs.getString("first_name"));
             }
-        }
-        catch (Exception e){
-            System.out.println("Exception occurred "+e);
+        } catch (Exception e) {
+            System.out.println("Exception occurred " + e);
         }
     }
 }
